5ff7c864d5f8e22a8aafc430992386e54ff80d29
daniw <dani.winz@bluewin.ch>
2015-01-01 00:38:04 +0100

    AH180N added to datasheets

--- datasheets/datasheets.sh
+++ datasheets/datasheets.sh
@@ -4,3 +4,8 @@
 
 # A3941
 wget -nc -O A3941.pdf http://www.allegromicro.com/~/media/Files/Datasheets/A3941-Datasheet.ashx
+
+### Sensors ###
+
+# AH180N -- Hall-Effect Switch
+wget -nc http://www.diodes.com/datasheets/AH180N.pdf
